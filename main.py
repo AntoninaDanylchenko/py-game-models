44bb712b6319bbaab8e93b82095b1c7272c291a3
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-05-18 13:13:21 +0200

    Add models and logic to populate database from JSON

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as f:
+        players = json.load(f)
 
+    for name, player in players.items():
+
+        race, _ = Race.objects.get_or_create(
+            name=player["race"]["name"],
+            defaults={"description": player["race"]["description"]}
+        )
+
+        if len(player["race"]["skills"]) > 0:
+            for skill in player["race"]["skills"]:
+                Skill.objects.get_or_create(
+                    name=skill["name"],
+                    defaults={
+                        "bonus": skill["bonus"],
+                        "race": race}
+                )
+
+        if player["guild"]:
+            guild, _ = Guild.objects.get_or_create(
+                name=player["guild"]["name"],
+                defaults={"description": player["guild"]["description"]}
+            )
+        else:
+            guild = None
+
+        Player.objects.create(
+            nickname=name,
+            email=player["email"],
+            bio=player["bio"],
+            race=race,
+            guild=guild
+        )
 
 if __name__ == "__main__":
     main()
